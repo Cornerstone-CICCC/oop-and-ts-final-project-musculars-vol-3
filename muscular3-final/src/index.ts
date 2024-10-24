107616504f17e15b212bf57d270b7e9e625dbf39
Evangelina Vega <evangelina1001@hotmail.com>
2024-10-24 14:23:34 -0700

    Edit functionality

--- muscular3-final/src/index.ts
+++ muscular3-final/src/index.ts
@@ -1,9 +1,13 @@
+import { validateEnvVariable } from "astro/env/runtime";
+
 type TaskList = {
-  id: number;
+  id: number | undefined;
   title: string;
   description: string;
   status: string;
 };
+
+type TaskArray= TaskList[]
 
 class Task {
   static taskId = 0;
@@ -20,8 +24,25 @@ class Task {
   }
 
   // update method
-  updateTask() {
-    return;
+  updateTask(id: number | undefined, data: TaskList): void {
+    const findTask = this.tasks.find((item) => {
+      return item.id === id;
+    });
+    console.log(findTask)
+    const updatedTask: TaskList= {
+      ...findTask,
+      id: data.id,
+      title: data.title, 
+      description: data.description,
+      status: data.status
+    }
+
+    const filteredTasks = this.tasks.filter((item) => {
+      return item.id !== item.id
+    })
+
+    this.tasks = [...filteredTasks, updatedTask]
+    this.renderTasks();
   }
 
   // delete method
@@ -80,20 +101,139 @@ class Task {
       newTask.classList.add("card");
 
       newTask.id = `task-${item.id}`;
+      newTask.setAttribute("todo-id", `${item.id}`)
       newTask.draggable = true;
 
       newTask.addEventListener("dragstart", (event) =>
-        this.onDrag(event, item.id)
+        this.onDrag(event, item.id as number)
       );
 
       newTask.innerHTML = `
       <div class="card-header">
         <h3 class="card-title">${item.title}</h3>
-        <img class="option" src="./ellipsis.png" alt="ellipsis">
+        <div>
+          <img class="edit" src="./edit.png" alt="edit icon" todo-id="${item.id}">
+          <img class="option" src="./delete.png" alt="delete icon">
+        </div>
       </div>
       <p class="card-desc">${item.description}</p>`;
 
       const option = newTask.querySelector(".option");
+      const edit  = newTask.querySelector(".edit");
+
+      if(edit){
+        edit.addEventListener("click", (e) => {
+          console.log(e.target)
+          const eventTarget = e.target as HTMLImageElement
+
+          const eventId = Number(eventTarget.getAttribute("todo-id"))
+
+          const overlayEdit = document.createElement("div")
+          overlayEdit.classList.add("overlay")
+          overlayEdit.innerHTML = `
+            <div class="edit-modal">
+              <div class="info">
+                <h2>Title</h2>
+                <img class="pencil-btn-title" src="./edit.png" alt="pencil icon">
+                <p>${item.title}</p>
+                <input type="text" class="edit-input-title">
+                <h2>Description</h2>
+                <img class="pencil-btn-description" src="./edit.png" alt="pencil icon">
+                <p>${item.description}</p>
+                <input type="text" class="edit-input-description">
+              </div>
+              <div class="assignees">
+                <h2>Assignees</h2>
+                <ul>
+                  <li><input type="checkbox"> User 1</li>
+                  <li><input type="checkbox"> User 2</li>
+                  <li><input type="checkbox"> User 3</li>
+                  <li><input type="checkbox"> User 4</li>
+                </ul>
+              </div>
+              <button class="save-btn">Save Changes</button>
+              <img class="close-btn" src="./close.png" alt="close icon">
+            </div> 
+          `
+          document.body.append(overlayEdit)
+
+          document.querySelector(".close-btn")?.addEventListener("click", () => {
+            this.renderTasks();
+          })
+          
+          document.querySelector(".pencil-btn-title")?.addEventListener("click", () => {
+            const inputTitle = document.querySelector<HTMLElement>(".edit-input-title")
+            const pencilTitle = document.querySelector<HTMLElement>(".pencil-btn-title");
+            if(inputTitle){
+              inputTitle.style.display = "block"
+            }
+            if(pencilTitle){
+              pencilTitle.style.display = "none"
+            }
+          })
+          document.querySelector(".pencil-btn-description")?.addEventListener("click", () => {
+            const inputDescription = document.querySelector<HTMLElement>(".edit-input-description")
+            const pencilDescription = document.querySelector<HTMLElement>(".pencil-btn-description");
+            if(inputDescription){
+              inputDescription.style.display = "block"
+            }
+            if(pencilDescription){
+              pencilDescription.style.display = "none"
+            }
+          })
+          
+
+          document.querySelector(".save-btn")?.addEventListener("click", () => {
+            const inputDescription = document.querySelector<HTMLElement>(".edit-input-description")
+            const inputTitle = document.querySelector<HTMLElement>(".edit-input-title")
+            const pencilDescription = document.querySelector<HTMLElement>(".pencil-btn-description");
+            const pencilTitle = document.querySelector<HTMLElement>(".pencil-btn-title")
+            const valueTitle = (<HTMLInputElement> document.querySelector(".edit-input-title")).value;
+            const valueDescription = (<HTMLInputElement> document.querySelector(".edit-input-description")).value;
+
+          
+            
+            this.updateTask(eventId, {
+              id: eventId,
+              title: valueTitle,
+              description: valueDescription,
+              status: item.status
+            })
+
+            if(!valueDescription){
+              this.updateTask(eventId, {
+                id: eventId,
+                title: valueTitle,
+                description: item.description,
+                status: item.status
+              })
+            } else if(!valueTitle){
+              this.updateTask(eventId, {
+                id: eventId,
+                title: item.title,
+                description: valueDescription,
+                status: item.status
+              })
+            }
+            
+            if(inputDescription){
+              inputDescription.style.display = "none"
+            }
+            if(pencilDescription){
+              pencilDescription.style.display = "block"
+            }
+            if(inputTitle){
+              inputTitle.style.display = "none"
+            }
+            if(pencilTitle){
+              pencilTitle.style.display = "block"
+            }
+            
+          })
+
+        })
+      }
+
       if (option) {
         option.addEventListener("click", (event) => {
           const overlay = document.createElement("div");
@@ -108,7 +248,7 @@ class Task {
           document.body.append(overlay);
 
           document.querySelector(".delete")?.addEventListener("click", () => {
-            let parent = event.target.parentNode.parentNode.id // todo : find a better way
+            let parent = event.target?.parentNode.parentNode.parentNode.id // todo : find a better way
               .toString()
               .substring(5);
             this.deleteTask(parseInt(parent));
